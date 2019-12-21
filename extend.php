9f429e74da6c7326c00fd281777c68386047ec16
pluveto <50045289+pluveto@users.noreply.github.com>
2019-12-21 21:47:30 +0800

    Create extend.php

---
+++ extend.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Bilibili
+ */
+use Flarum\Extend;
+use s9e\TextFormatter\Configurator;
+return [
+    (new Extend\Formatter)
+        ->configure(function (Configurator $config) {
+             $config->BBCodes->addCustom(
+               '[bilibili aid={NUMBER1?} cid={NUMBER2?}][/bilibili]',
+               '<iframe style="height: 40em; width: 100%" src="//player.bilibili.com/player.html?aid={NUMBER1}&cid={NUMBER2}&page=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>'
+            );
+        })
+];
